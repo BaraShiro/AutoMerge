30df3f48db74679a5d4b96d8f18e4b20a01b9d3e
Robert Rosborg <rosborg.robert@gmail.com>
2019-04-16 15:36:03 +0200

    MSE implemented

--- AutoMerge.py
+++ AutoMerge.py
@@ -23,15 +23,16 @@ def get_frames(start: int, end: int, video: cv.VideoCapture) -> List[np.ndarray]
     return out
 
 
-# TODO: catch file not found exceptions
+# Finds the most similar frames in the end of the lead vid and the bigining of the following vid
 def find_matching_frames(lead_vid_path: str, following_vids_paths: List[str],
                          seconds: int = 3, method: str = '') -> List[Tuple[int, int, float]]:
+    # TODO: catch file not found exceptions
     capture: cv.VideoCapture = cv.VideoCapture(lead_vid_path)
     number_of_frames: float = capture.get(cv.CAP_PROP_FRAME_COUNT)
     fps: float = capture.get(cv.CAP_PROP_FPS)
     lead_vid_start: int = int(number_of_frames) - int((fps * seconds)) - 1
     lead_vid_end: int = int(number_of_frames) - 1
-    print(number_of_frames, fps, lead_vid_start)
+    print(number_of_frames, fps, lead_vid_start, lead_vid_end)
     lead_vid: List[np.ndarray] = get_frames(lead_vid_start, lead_vid_end, capture)
     capture.release()
 
@@ -45,11 +46,15 @@ def find_matching_frames(lead_vid_path: str, following_vids_paths: List[str],
 
     out: List[Tuple[int, int, float]] = []
     for following_vid in following_vids:
-        min_diff = (0, 0, 0.0) # TODO: confusing names, diff etc.?
+        print("One vid")
+
+        # Refactor this
+        min_diff = (0, 0, float('inf'))  # TODO: confusing names, diff etc.?
         for i, lead_frame in enumerate(lead_vid):
+            print("frame ", i)
             for j, following_frame in enumerate(following_vid):
                 diff: float = get_image_difference(lead_frame, following_frame, method)
-                if diff > min_diff[2]:
+                if diff <= min_diff[2]:
                     min_diff = (i, j, diff)
         out.append(min_diff)
 
@@ -57,7 +62,15 @@ def find_matching_frames(lead_vid_path: str, following_vids_paths: List[str],
 
 
 def get_image_difference(img_1: np.ndarray, img_2: np.ndarray, method: str = '') -> float:
-    return 0.5
+    return mean_square_error(img_1, img_2)
+
+
+def mean_square_error(lead_frame: np.ndarray, following_frame: np.ndarray) -> np.float64:
+    error_sum: np.ndarray = np.sum((lead_frame.astype('float') - following_frame.astype('float')) ** 2, dtype=np.float64)
+    image_size: int = lead_frame.shape[0] * lead_frame.shape[1]  # Assumes both images are the same size
+    mean_error: np.float64 = error_sum / (image_size)
+    return mean_error
+
 
 
 def main():
@@ -77,4 +90,8 @@ def main():
 
 
 # main()
-print(find_matching_frames('Test/red_blue_test.avi', ['Test/m.mkv', 'Test/m.mkv']))
+print(find_matching_frames('Test/red_frame_test_1.avi', ['Test/red_frame_test_2.avi']))
+# mao = cv.imread('Test/mao.jpg')
+# maogray = cv.imread('Test/maogray.png')
+# err = mean_square_error(mao, maogray)
+# print(err)
