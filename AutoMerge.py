fixed an off by one error

AutoMerge.py
@@ -7,19 +7,25 @@ from matplotlib import pyplot as plt
 
 
 # Gets frames from start to end from video at filepath and returns the in a list
-def get_frames(start: int, end: int, video: cv.VideoCapture) -> List[np.ndarray]:
+def get_frames(start: int, number_of_frames_to_read: int, video: cv.VideoCapture) -> List[np.ndarray]:
     # Init variables
     out: List[np.ndarray] = []
     video.set(cv.CAP_PROP_POS_FRAMES, start)  # Jump to start frame
+    cv.namedWindow('frame', cv.WINDOW_NORMAL)
 
     # Read frames from the file, if reading the frame is successful append it to the list, else stop reading frames
-    for x in range(end - start + 1):
+    print(start, start + number_of_frames_to_read - 1,  number_of_frames_to_read)
+    for x in range(number_of_frames_to_read):
         success, frame = video.read()
         if success:
+            cv.imshow('frame', frame)
             out.append(frame)
+            k = cv.waitKey(0) & 0xFF
+
         else:
             break
 
+    cv.destroyAllWindows()
     return out
 
 
@@ -28,34 +34,34 @@ def find_matching_frames(lead_vid_path: str, following_vids_paths: List[str],
                          seconds: int = 3, method: str = '') -> List[Tuple[int, int, float]]:
     # TODO: catch file not found exceptions
     capture: cv.VideoCapture = cv.VideoCapture(lead_vid_path)
-    number_of_frames: float = capture.get(cv.CAP_PROP_FRAME_COUNT)
-    fps: float = capture.get(cv.CAP_PROP_FPS)
-    lead_vid_start: int = int(number_of_frames) - int((fps * seconds)) - 1
-    lead_vid_end: int = int(number_of_frames) - 1
-    print(number_of_frames, fps, lead_vid_start, lead_vid_end)
-    lead_vid: List[np.ndarray] = get_frames(lead_vid_start, lead_vid_end, capture)
+    number_of_frames: int = int(capture.get(cv.CAP_PROP_FRAME_COUNT))
+    fps: int = int(capture.get(cv.CAP_PROP_FPS))
+    number_of_frames_to_read: int = fps * seconds
+    lead_vid_start: int = number_of_frames - number_of_frames_to_read
+    print("First vid starting at frame", lead_vid_start, "and ending at frame",
+          lead_vid_start + number_of_frames_to_read - 1, ", number of frames to read", number_of_frames_to_read)
+    lead_vid: List[np.ndarray] = get_frames(lead_vid_start, number_of_frames_to_read, capture)
     capture.release()
 
     following_vids: List[List[np.ndarray]] = []
     for path in following_vids_paths:
         capture: cv.VideoCapture = cv.VideoCapture(path)
-        fps: float = capture.get(cv.CAP_PROP_FPS)
-        following_vid_end: int = int((fps * seconds)) - 1
-        following_vids.append(get_frames(0, following_vid_end, capture))
+        fps: int = int(capture.get(cv.CAP_PROP_FPS))
+        number_of_frames_to_read: int = fps * seconds
+        following_vids.append(get_frames(0, number_of_frames_to_read, capture))
         capture.release()
 
     out: List[Tuple[int, int, float]] = []
     for following_vid in following_vids:
-        print("One vid")
 
         # Refactor this
         min_diff = (0, 0, float('inf'))  # TODO: confusing names, diff etc.?
         for i, lead_frame in enumerate(lead_vid):
-            print("frame ", i)
+            print("Processing frame", i)
             for j, following_frame in enumerate(following_vid):
                 diff: float = get_image_difference(lead_frame, following_frame, method)
-                if diff <= min_diff[2]:
-                    min_diff = (i, j, diff)
+                if diff < min_diff[2]:
+                    min_diff = (i + lead_vid_start, j, diff)
         out.append(min_diff)
 
     return out
@@ -91,6 +97,7 @@ def main():
 
 # main()
 print(find_matching_frames('Test/red_frame_test_1.avi', ['Test/red_frame_test_2.avi']))
+# print(find_matching_frames('Test/m.mkv', ['Test/m.mkv']))
 # mao = cv.imread('Test/mao.jpg')
 # maogray = cv.imread('Test/maogray.png')
 # err = mean_square_error(mao, maogray)