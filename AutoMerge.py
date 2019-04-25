ba4acba7b2d68f558721b2ed36661f43bedc1734
Robert Rosborg <rosborg.robert@gmail.com>
2019-04-25 14:41:11 +0200

    Using mse, nrmse, psnr, ssim from scikit-image

--- AutoMerge.py
+++ AutoMerge.py
@@ -3,6 +3,7 @@ import os
 from typing import *
 import numpy as np
 import cv2 as cv
+from skimage.measure import compare_mse, compare_nrmse, compare_psnr, compare_ssim
 from matplotlib import pyplot as plt
 
 
@@ -11,38 +12,33 @@ def get_frames(start: int, number_of_frames_to_read: int, video: cv.VideoCapture
     # Init variables
     out: List[np.ndarray] = []
     video.set(cv.CAP_PROP_POS_FRAMES, start)  # Jump to start frame
-    cv.namedWindow('frame', cv.WINDOW_NORMAL)
 
     # Read frames from the file, if reading the frame is successful append it to the list, else stop reading frames
-    print(start, start + number_of_frames_to_read - 1,  number_of_frames_to_read)
     for x in range(number_of_frames_to_read):
         success, frame = video.read()
         if success:
-            cv.imshow('frame', frame)
             out.append(frame)
-            k = cv.waitKey(0) & 0xFF
 
         else:
             break
 
-    cv.destroyAllWindows()
     return out
 
 
-# Finds the most similar frames in the end of the lead vid and the bigining of the following vid
+# Finds the most similar frames in the end of the lead vid and the beginning of the following vid
 def find_matching_frames(lead_vid_path: str, following_vids_paths: List[str],
-                         seconds: int = 3, method: str = '') -> List[Tuple[int, int, float]]:
+                         seconds: int = 3, method: str = 'mse') -> List[Tuple[int, int, float]]:
     # TODO: catch file not found exceptions
+    # Get lead video
     capture: cv.VideoCapture = cv.VideoCapture(lead_vid_path)
     number_of_frames: int = int(capture.get(cv.CAP_PROP_FRAME_COUNT))
     fps: int = int(capture.get(cv.CAP_PROP_FPS))
     number_of_frames_to_read: int = fps * seconds
     lead_vid_start: int = number_of_frames - number_of_frames_to_read
-    print("First vid starting at frame", lead_vid_start, "and ending at frame",
-          lead_vid_start + number_of_frames_to_read - 1, ", number of frames to read", number_of_frames_to_read)
     lead_vid: List[np.ndarray] = get_frames(lead_vid_start, number_of_frames_to_read, capture)
     capture.release()
 
+    # Get following videos
     following_vids: List[List[np.ndarray]] = []
     for path in following_vids_paths:
         capture: cv.VideoCapture = cv.VideoCapture(path)
@@ -51,32 +47,61 @@ def find_matching_frames(lead_vid_path: str, following_vids_paths: List[str],
         following_vids.append(get_frames(0, number_of_frames_to_read, capture))
         capture.release()
 
+    # Calculate most similar frames
     out: List[Tuple[int, int, float]] = []
     for following_vid in following_vids:
-
-        # Refactor this
-        min_diff = (0, 0, float('inf'))  # TODO: confusing names, diff etc.?
-        for i, lead_frame in enumerate(lead_vid):
-            print("Processing frame", i)
-            for j, following_frame in enumerate(following_vid):
-                diff: float = get_image_difference(lead_frame, following_frame, method)
-                if diff < min_diff[2]:
-                    min_diff = (i + lead_vid_start, j, diff)
-        out.append(min_diff)
+        most_similar_frames: Tuple[int, int, float] = get_most_similar_frames(lead_vid, following_vid, lead_vid_start, method)
+        out.append(most_similar_frames)
 
     return out
 
 
-def get_image_difference(img_1: np.ndarray, img_2: np.ndarray, method: str = '') -> float:
-    return mean_square_error(img_1, img_2)
+def get_most_similar_frames(lead_vid: List[np.ndarray], following_vid: List[np.ndarray],
+                               offset: int, method: str = 'mse') -> (int, int, float):
+    if method == 'mse':
+        min_diff = (0, 0, float('inf'))
+        for i, lead_frame in enumerate(lead_vid):
+            print("Processing frame", i, "of", len(lead_vid) - 1)
+            for j, following_frame in enumerate(following_vid):
+                # diff: float = mean_square_error(lead_frame, following_frame)
+                diff: float = compare_mse(lead_frame, following_frame)
+                if diff < min_diff[2]:
+                    min_diff = (i + offset, j, diff)
+        return min_diff
 
+    elif method == 'nrmse':
+        min_diff = (0, 0, float('inf'))
+        for i, lead_frame in enumerate(lead_vid):
+            print("Processing frame", i, "of", len(lead_vid) - 1)
+            for j, following_frame in enumerate(following_vid):
+                diff: float = compare_nrmse(lead_frame, following_frame)
+                if diff < min_diff[2]:
+                    min_diff = (i + offset, j, diff)
+        return min_diff
 
-def mean_square_error(lead_frame: np.ndarray, following_frame: np.ndarray) -> np.float64:
-    error_sum: np.ndarray = np.sum((lead_frame.astype('float') - following_frame.astype('float')) ** 2, dtype=np.float64)
-    image_size: int = lead_frame.shape[0] * lead_frame.shape[1]  # Assumes both images are the same size
-    mean_error: np.float64 = error_sum / (image_size)
-    return mean_error
+    elif method == 'psnr':
+        min_diff = (0, 0, float('-inf'))
+        for i, lead_frame in enumerate(lead_vid):
+            print("Processing frame", i, "of", len(lead_vid) - 1)
+            for j, following_frame in enumerate(following_vid):
+                diff: float = compare_psnr(lead_frame, following_frame)
+                if diff > min_diff[2]:
+                    min_diff = (i + offset, j, diff)
+        return min_diff
 
+    elif method == 'ssim':
+        min_diff = (0, 0, float('-inf'))
+        for i, lead_frame in enumerate(lead_vid):
+            print("Processing frame", i, "of", len(lead_vid) - 1)
+            for j, following_frame in enumerate(following_vid):
+                diff = compare_ssim(lead_frame, following_frame, multichannel=True, full=False) # Color: yes, return imgae: no
+                if diff > min_diff[2]:
+                    min_diff = (i + offset, j, diff)
+
+        return min_diff
+    else:
+        print("Invalid method, defaulting to MSE")
+        return get_most_similar_frames(lead_vid, following_vid, offset, 'mse')
 
 
 def main():
@@ -96,7 +121,7 @@ def main():
 
 
 # main()
-print(find_matching_frames('Test/red_frame_test_1.avi', ['Test/red_frame_test_2.avi']))
+print(find_matching_frames('Test/red_frame_test_1.avi', ['Test/red_frame_test_2.avi'], method='ssim'))
 # print(find_matching_frames('Test/m.mkv', ['Test/m.mkv']))
 # mao = cv.imread('Test/mao.jpg')
 # maogray = cv.imread('Test/maogray.png')
