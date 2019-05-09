7520c844dd621299c5eece6ecab9d0f0951482cb
Robert Rosborg <rosborg.robert@gmail.com>
2019-05-09 19:50:00 +0200

    Improved GUI layout

--- GUI.py
+++ GUI.py
@@ -6,63 +6,81 @@ from AutoMerge import find_matching_frames
 
 class App:
 
-    def __init__(self, master):
+    def __init__(self, window):
 
+        # Variables to be passed to find_matching_frames
         self.colour = BooleanVar()
         self.mode = StringVar()
         self.seconds = IntVar()
 
-        Label(master, text="Leading:").grid(row=0, column=0, sticky=W)
+        # The main frame, mainly for the padding
+        self.main = Frame(window, padx=10, pady=10)
+        self.main.grid(row=0, column=0)
 
-        self.lead_entry = Entry(master, width=100, state='readonly')
-        self.lead_entry.grid(row=0, column=1)
+        # The file selection frame
+        self.files_frame = LabelFrame(self.main, text="Files", padx=5, pady=5)
+        self.files_frame.grid(row=0, column=0, sticky=NW)
 
-        self.lead_add_button = Button(master, text="+", width=3, command=self.browse_lead)
-        self.lead_add_button.grid(row=0, column=3, padx=5, pady=5, sticky=NW)
+        Frame(self.main, width=10, padx=10, pady=10).grid(row=0, column=1)  # Separator
 
-        Label(master, text="Following:").grid(row=1, column=0, sticky=NW)
+        # The options frame
+        self.options_frame = LabelFrame(self.main, text="Options", padx=5, pady=5)
+        self.options_frame.grid(row=0, column=2, sticky=NW)
 
-        self.following_scrollbar = Scrollbar(master, orient=VERTICAL)
-        self.following_scrollbar.grid(row=1, column=2, rowspan=3, sticky=N+S)
+        # The go button
+        self.go = Button(self.main, text="Go!", width=10, command=self.go)
+        self.go.grid(row=1, column=2, sticky=NE)
 
-        self.following_listbox = Listbox(master, selectmode=SINGLE, width=100, yscrollcommand=self.following_scrollbar.set)
-        self.following_listbox.grid(row=1, column=1, rowspan=3)
+        # Widgets that go into the file selection frame
+        Label(self.files_frame, text="Leading:").grid(row=0, column=0, sticky=W)
 
-        self.following_scrollbar.config(command=self.following_listbox.yview)
+        self.lead_entry = Entry(self.files_frame, width=100, state='readonly')
+        self.lead_entry.grid(row=1, column=0)
 
-        self.following_add_button = Button(master, text="+", width=3, command=self.browse_following)
-        self.following_add_button.grid(row=1, column=3, padx=5, pady=5, sticky=NW)
+        self.lead_add_button = Button(self.files_frame, text="...", width=3, command=self.browse_lead)
+        self.lead_add_button.grid(row=1, column=2, padx=5, pady=5, sticky=NW)
 
-        self.following_sub_button = Button(master, text="-", width=3,
+        Label(self.files_frame, text="Following:").grid(row=2, column=0, sticky=NW)
+
+        self.following_scrollbar = Scrollbar(self.files_frame, orient=VERTICAL)
+        self.following_scrollbar.grid(row=3, column=1, rowspan=3, sticky=N+S)
+
+        self.following_listbox = Listbox(self.files_frame, selectmode=SINGLE, width=100, yscrollcommand=self.following_scrollbar.set)
+        self.following_listbox.grid(row=3, column=0, rowspan=3)
+
+        self.following_scrollbar.config(command=self.following_listbox.yview)  # Connect the scrollbar to the listbox
+
+        self.following_add_button = Button(self.files_frame, text="+", width=3, command=self.browse_following)
+        self.following_add_button.grid(row=3, column=2, padx=5, pady=5, sticky=NW)
+
+        self.following_sub_button = Button(self.files_frame, text="-", width=3,
                                            command=lambda sub=self.following_listbox: self.following_listbox.delete(ANCHOR))
-        self.following_sub_button.grid(row=2, column=3, padx=5, pady=5, sticky=NW)
+        self.following_sub_button.grid(row=4, column=2, padx=5, pady=5, sticky=NW)
 
-        self.go = Button(master, text="Go!", command=self.go)
-        self.go.grid(row=4, columnspan=2, sticky=E+W, padx=5, pady=5)
+        # Widgets that go into the options frame
+        self.colour_check = Checkbutton(self.options_frame, text="Colour", variable=self.colour)
+        self.colour_check.grid(row=0, column=0)
 
-        self.test_button = Button(master, text="Test", command=self.test)
-        self.test_button.grid(row=4, column=3, sticky=E+W, padx=5, pady=5)
+        Label(self.options_frame, text="Algorithm:").grid(row=1, column=0, sticky=NW)
 
-        self.colour_check = Checkbutton(master, text="Colour", variable=self.colour)
-        self.colour_check.grid(row=0, column=4)
-
-        self.mse_radio_button = Radiobutton(master, text="MSE", variable=self.mode, value="mse")
-        self.mse_radio_button.grid(row=1, column=4,sticky=NW)
+        self.mse_radio_button = Radiobutton(self.options_frame, text="MSE", variable=self.mode, value="mse")
+        self.mse_radio_button.grid(row=2, column=0,sticky=NW)
         self.mse_radio_button.select()
 
-        self.nrmse_radio_button = Radiobutton(master, text="NRMSE", variable=self.mode, value="nrmse")
-        self.nrmse_radio_button.grid(row=1, column=5, sticky=NW)
+        self.nrmse_radio_button = Radiobutton(self.options_frame, text="NRMSE", variable=self.mode, value="nrmse")
+        self.nrmse_radio_button.grid(row=2, column=1, sticky=NW)
 
-        self.psnr_radio_button = Radiobutton(master, text="PSNR", variable=self.mode, value="psnr")
-        self.psnr_radio_button.grid(row=2, column=4, sticky=NW)
+        self.psnr_radio_button = Radiobutton(self.options_frame, text="PSNR", variable=self.mode, value="psnr")
+        self.psnr_radio_button.grid(row=3, column=0, sticky=NW)
 
-        self.ssim_radio_button = Radiobutton(master, text="SSIM", variable=self.mode, value="ssim")
-        self.ssim_radio_button.grid(row=2, column=5, sticky=NW)
+        self.ssim_radio_button = Radiobutton(self.options_frame, text="SSIM", variable=self.mode, value="ssim")
+        self.ssim_radio_button.grid(row=3, column=1, sticky=NW)
 
-        self.seconds_spinbox = Spinbox(master, from_=1, to=10, textvariable=self.seconds, state='readonly')
-        self.seconds_spinbox.grid(row=3, column=4, columnspan=2, sticky=NW)
+        Label(self.options_frame, text="Seconds:").grid(row=4, column=0, sticky=NW)
+
+        self.seconds_spinbox = Spinbox(self.options_frame, from_=1, to=10, textvariable=self.seconds, state='readonly')
+        self.seconds_spinbox.grid(row=5, column=0, columnspan=2, sticky=NW)
         self.seconds.set(3)
-
 
     def browse_lead(self):
         file_path = filedialog.askopenfilename(title="Select file",
@@ -76,9 +94,6 @@ class App:
         file_path = filedialog.askopenfilename(title="Select file",
                                                    filetypes=(("Video files", "*.avi *.mp4 *.mkv"), ("all files", "*.*")))
         self.following_listbox.insert(END, file_path)
-
-    def test(self):
-        print(type(self.seconds.get()))
 
     def go(self):
         leading_vid = self.lead_entry.get()
@@ -105,12 +120,10 @@ class App:
         return
 
 
-root = Tk()  # Create main window
-root.title("AutoMerge")  # Give the main window a name
+main_window = Tk()  # Create main window
+main_window.title("AutoMerge")  # Give the main window a name
 
-app = App(root)  # Create the App object
+app = App(main_window)  # Create the App object
 
-root.mainloop()  # Start
-# root.destroy()  # Maybe needed on Linux for graceful exit, throws an exception on Windows
-
-
+main_window.mainloop()  # Start
+# main_window.destroy()  # Maybe needed on Linux for graceful exit, throws an exception on Windows
