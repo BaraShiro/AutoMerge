c487709d4cab6f310e207f51005c8b626b05a0eb
Robert Rosborg <rosborg.robert@gmail.com>
2019-05-16 02:41:48 +0200

    Parallelised get_most_similar_frames

--- AutoMerge.py
+++ AutoMerge.py
@@ -6,6 +6,7 @@ import cv2 as cv
 from skimage.measure import compare_mse, compare_nrmse, compare_psnr, compare_ssim
 from matplotlib import pyplot as plt
 import time
+from joblib import Parallel, delayed
 
 
 # Gets frames from start to end from video at filepath and returns the in a list
@@ -33,6 +34,7 @@ def get_frames(start: int, number_of_frames_to_read: int, video: cv.VideoCapture
 def find_matching_frames(lead_vid_path: str, following_vids_paths: List[str], seconds: int = 3,
                          multichannel: bool = True,  method: str = 'mse') -> List[Tuple[int, int, float]]:
     # TODO: catch file not found exceptions
+    # TODO: check for seconds longer that video length
     # Get lead video
     capture: cv.VideoCapture = cv.VideoCapture(lead_vid_path)
     number_of_frames: int = int(capture.get(cv.CAP_PROP_FRAME_COUNT))
@@ -61,48 +63,95 @@ def find_matching_frames(lead_vid_path: str, following_vids_paths: List[str], se
     return out
 
 
+def run_mse(lead_frame: np.ndarray, following_frame: np.ndarray,
+            lead_frame_number: int, following_frame_number: int) -> Tuple[int, int, float]:
+    score: float = compare_mse(lead_frame, following_frame)
+    return lead_frame_number, following_frame_number, score
+
+
+def run_nrmse(lead_frame: np.ndarray, following_frame: np.ndarray,
+              lead_frame_number: int, following_frame_number: int) -> Tuple[int, int, float]:
+    score: float = compare_nrmse(lead_frame, following_frame)
+    return lead_frame_number, following_frame_number, score
+
+
+def run_psnr(lead_frame: np.ndarray, following_frame: np.ndarray,
+             lead_frame_number: int, following_frame_number: int) -> Tuple[int, int, float]:
+    score: float = compare_psnr(lead_frame, following_frame)
+    return lead_frame_number, following_frame_number, score
+
+
+def run_ssim(lead_frame: np.ndarray, following_frame: np.ndarray,
+             lead_frame_number: int, following_frame_number: int, multichannel: bool = True) -> Tuple[int, int, float]:
+    score: float = compare_ssim(lead_frame, following_frame, multichannel=multichannel)
+    return lead_frame_number, following_frame_number, score
+
+
 def get_most_similar_frames(lead_vid: List[np.ndarray], following_vid: List[np.ndarray],
                                offset: int, multichannel: bool = True, method: str = 'mse') -> (int, int, float):
+    start = time.time()
+
+    number_of_jobs: int = os.cpu_count()  # Try to set number of jobs to the number of available CPUs
+    if not number_of_jobs:  # If os.cpu_count() failed and returned None
+        number_of_jobs = 4  # Default to 4 jobs, as that's good enough
+
     if method == 'mse':
-        min_diff = (0, 0, float('inf'))
-        for i, lead_frame in enumerate(lead_vid):
-            print("Processing frame", i, "of", len(lead_vid) - 1)
-            for j, following_frame in enumerate(following_vid):
-                diff: float = compare_mse(lead_frame, following_frame)
-                if diff < min_diff[2]:
-                    min_diff = (i + offset, j, diff)
+        min_diff: Tuple[int, int, float] = (0, 0, float('inf'))
+        with Parallel(n_jobs=number_of_jobs, prefer="threads") as parallel:
+            for i, lead_frame in enumerate(lead_vid):
+                print("Processing frame", i, "of", len(lead_vid) - 1)
+                diff_list: List[Tuple[int, int, float]] = (parallel(delayed(run_mse)(lead_frame, following_frame, i + offset, j)
+                                                                    for j, following_frame in enumerate(following_vid)))
+                diff: Tuple[int, int, float] = min(diff_list, key=lambda diff_tuple: diff_tuple[2])
+                if diff[2] < min_diff[2]:
+                    min_diff = diff
+
+            end = time.time()
+        print('Time elapsed:', end - start)
         return min_diff
 
     elif method == 'nrmse':
-        min_diff = (0, 0, float('inf'))
-        for i, lead_frame in enumerate(lead_vid):
-            print("Processing frame", i, "of", len(lead_vid) - 1)
-            for j, following_frame in enumerate(following_vid):
-                diff: float = compare_nrmse(lead_frame, following_frame)
-                if diff < min_diff[2]:
-                    min_diff = (i + offset, j, diff)
+        min_diff: Tuple[int, int, float] = (0, 0, float('inf'))
+        with Parallel(n_jobs=number_of_jobs, prefer="threads") as parallel:
+            for i, lead_frame in enumerate(lead_vid):
+                print("Processing frame", i, "of", len(lead_vid) - 1)
+                diff_list: List[Tuple[int, int, float]] = (parallel(delayed(run_nrmse)(lead_frame, following_frame, i + offset, j)
+                                                                    for j, following_frame in enumerate(following_vid)))
+                diff: Tuple[int, int, float] = min(diff_list, key=lambda diff_tuple: diff_tuple[2])
+                if diff[2] < min_diff[2]:
+                    min_diff = diff
+
+            end = time.time()
+        print('Time elapsed:', end - start)
         return min_diff
 
     elif method == 'psnr':
-        min_diff = (0, 0, float('-inf'))
-        for i, lead_frame in enumerate(lead_vid):
-            print("Processing frame", i, "of", len(lead_vid) - 1)
-            for j, following_frame in enumerate(following_vid):
-                diff: float = compare_psnr(lead_frame, following_frame)
-                if diff > min_diff[2]:
-                    min_diff = (i + offset, j, diff)
+        min_diff: Tuple[int, int, float] = (0, 0, float('-inf'))
+        with Parallel(n_jobs=number_of_jobs, prefer="threads") as parallel:
+            for i, lead_frame in enumerate(lead_vid):
+                print("Processing frame", i, "of", len(lead_vid) - 1)
+                diff_list: List[Tuple[int, int, float]] = (parallel(delayed(run_psnr)(lead_frame, following_frame, i + offset, j)
+                                                                    for j, following_frame in enumerate(following_vid)))
+                diff: Tuple[int, int, float] = max(diff_list, key=lambda diff_tuple: diff_tuple[2])
+                if diff[2] > min_diff[2]:
+                    min_diff = diff
+
+            end = time.time()
+        print('Time elapsed:', end - start)
         return min_diff
 
     elif method == 'ssim':
-        min_diff = (0, 0, float('-inf'))
-        start = time.time()
-        for i, lead_frame in enumerate(lead_vid):
-            print("Processing frame", i, "of", len(lead_vid) - 1)
-            for j, following_frame in enumerate(following_vid):
-                diff = compare_ssim(lead_frame, following_frame, multichannel=multichannel)
-                if diff > min_diff[2]:
-                    min_diff = (i + offset, j, diff)
-        end = time.time()
+        min_diff: Tuple[int, int, float] = (0, 0, float('-inf'))
+        with Parallel(n_jobs=number_of_jobs, prefer="threads") as parallel:
+            for i, lead_frame in enumerate(lead_vid):
+                print("Processing frame", i, "of", len(lead_vid) - 1)
+                diff_list: List[Tuple[int, int, float]] = (parallel(delayed(run_ssim)(lead_frame, following_frame, i + offset, j, multichannel=multichannel)
+                                                                    for j, following_frame in enumerate(following_vid)))
+                diff: Tuple[int, int, float] = max(diff_list, key=lambda diff_tuple: diff_tuple[2])
+                if diff[2] > min_diff[2]:
+                    min_diff = diff
+
+            end = time.time()
         print('Time elapsed:', end - start)
         return min_diff
 
@@ -128,9 +177,10 @@ def main():
 
 
 # main()
-print(find_matching_frames('Test/red_frame_test_1.avi', ['Test/red_frame_test_2.avi'], seconds=2, multichannel=False, method='mse'))
+# print(find_matching_frames('Test/red_frame_test_1.avi', ['Test/red_frame_test_2.avi'], seconds=2, multichannel=False, method='mse'))
 # print(find_matching_frames('Test/m.mkv', ['Test/m.mkv']))
 # mao = cv.imread('Test/mao.jpg')
 # maogray = cv.imread('Test/maogray.png')
 # err = mean_square_error(mao, maogray)
 # print(err)
+#print(find_matching_frames('Test/red_frame_test_1.avi', ['Test/red_frame_test_2.avi'], seconds=1, multichannel=False, method='psnr'))
